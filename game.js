5298d86f5d4b0be2bc7413861a7d597eb9d2800e
PhillMue <muenzuve@gmail.com>
2016-02-07 21:36:57 +0300

    working

---
+++ game.js
@@ -0,0 +1,18 @@
+// Check if the user is ready to play!
+
+confirm("ready")
+
+var age = prompt ("What's your age?");
+
+if (age < 13){
+
+    console.log("unallowed")
+}
+
+else 
+
+{
+
+    console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'")
+    
+}
